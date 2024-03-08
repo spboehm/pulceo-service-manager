1ac7ecb3ca8ff4de696ddfdc3945bb2d18028643
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-03-08 18:31:29 +0100

    update health controller

--- src/main/java/dev/pulceo/prm/controller/HealthController.java
+++ src/main/java/dev/pulceo/prm/controller/HealthController.java
@@ -1,14 +1,14 @@
 package dev.pulceo.prm.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
 public class HealthController {
 
-        @RequestMapping(value = {"/health", "/healthz", "/psm/health"})
+        @GetMapping(value = {"/health", "/healthz", "/psm/health"})
         public ResponseEntity<String> health() {
             return ResponseEntity.status(200).body("OK");
         }
